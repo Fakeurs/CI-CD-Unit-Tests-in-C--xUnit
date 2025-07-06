c0f648d4dc41b4a72efb6754843629236f31937f
Ridwan <ridghamy@gmail.com>
2025-07-06 15:35:03 +0400

    Add method created

---
+++ GitHubActions.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace GitHubActions.Test;
+using GitHubActions;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(2, 3,5)]
+    [InlineData(5, 7,12)]
+    [InlineData(2, 0,20)]
+    public void Sum_Of_Two_Numbers(int num1, int num2, int sum)
+    {
+        // Act
+        var result = Program.Add(num1, num2);
+        
+        // Assert
+        Assert.Equal(sum,result); // Expected, Actual
+    }
+}
---
+++ GitHubActions/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GitHubActions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Add(2,3));
+    }
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
